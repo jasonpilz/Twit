c810e8e0d039d2c92e79f70fa397964567beb2f8
Jason Pilz <jasonpilz@gmail.com>
2015-12-14 18:27:36 -0700

    Implement oauth authentication

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,57 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'capybara/rails'
+require 'mocha/mini_test'
+require 'simplecov'
+require 'minitest/pride'
+
+SimpleCov.start
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+end
 
-  # Add more helper methods to be used by all tests here...
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
+
+  def setup
+    Capybara.app = Twit::Application
+    stub_omniauth
+  end
+
+  def teardown
+    reset_session!
+  end
+
+  def stub_omniauth
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+      provider: 'twitter',
+      extra: {
+        raw_info: {
+          user_id: "1234",
+          name: "Jason",
+          screen_name: "JasonPilz",
+        }
+      },
+      credentials: {
+        token: "pizza",
+        secret: "secretpizza"
+      }
+    })
+  end
+end
+
+DatabaseCleaner.strategy = :transaction
+
+class Minitest::Spec
+  def setup
+    DatabaseCleaner.start
+  end
+
+  def teardown
+    DatabaseCleaner.clean
+  end
 end
